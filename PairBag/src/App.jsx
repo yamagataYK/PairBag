dadced965771dd20e150adaa9493b0cc0f9d1746
Yamagata Kaito <yamagatakaito0921@gmail.com>
2025-07-11 18:39:00 +0900

    img

--- PairBag/src/App.jsx
+++ PairBag/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, } from 'react-router-dom';
+import Header from "./layouts/Header";
+import Footer from './layouts/Footer';
+import Profile from "./pages/Profile";
+import Notice from "./pages/Notice";
+import Notification from "./pages/Notification";
+import Index from './pages/Index';
+import Camera from './pages/Camera';
+import SignUp from './pages/SignUp';
+import Chat from './pages/Chat';
+import ChatFooter from './layouts/ChatFooter';
+import MainHeader from './layouts/MainHeader';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      {/* 1. 単独ページ */}
+      <Route path="/sign-up" element={<SignUp />} />
+
+      {/* 2. Footer付き共通レイアウト */}
+      <Route element={<Footer />}>
+
+        {/* MainHeaderグループ */}
+        <Route element={<MainHeader />}>
+          <Route index element={<Index />} />
+          <Route path="/camera" element={<Camera />} />
+        </Route>
+
+        {/* Headerグループ (chat以外) */}
+        <Route element={<Header />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/notice" element={<Notice />} />
+          <Route path="/notifications" element={<Notification />} />
+        </Route>
+
+      </Route>
+
+      {/* 3. Chatページ専用：Header + ChatFooter */}
+      <Route element={<ChatFooter />}>
+        <Route element={<Header />}>
+          <Route path="/chat" element={<Chat />} />
+        </Route>
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
